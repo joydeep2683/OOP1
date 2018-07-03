69f982f86315af7567effe91b3c9ce4c57cccd99
Joydeep Ballabh <joydeep2683@gmail.com>
2018-07-03 14:46:52 +0530

    class varable issues

--- oop.py
+++ oop.py
@@ -3,21 +3,31 @@
 
 
 class Employee:
+
+
+	num_of_emps = 0
+	raise_amount = 1.04
 	def __init__(self, first, last, pay):
 		self.first = first
 		self.last = last
 		self.pay = pay
 		self.email = first + "." + last + "@company.com"
 
+		Employee.num_of_emps += 1
+
 	"""For printing full name of employee"""
 	def fullname(self):
 		return f'{self.first} {self.last}'
 
 	def apply_raise(self):
-		self.pay = int(self.pay * 1.04)
+		self.pay = int(self.pay * self.raise_amount)
+
+print(Employee.num_of_emps)
 
 emp_1 = Employee('Joy', 'Deep', 50000)
 emp_2 = Employee('Jony', 'Deap', 60000)
+
+print(Employee.num_of_emps)
 
 # print(emp_1)
 # print(emp_2)
@@ -45,6 +55,19 @@ print(emp_1.pay)
 emp_1.apply_raise()
 print(emp_1.pay)
 
+print("raise_amount variable created")
+
+print(Employee.raise_amount)
+print(emp_1.raise_amount)
+print(emp_2.raise_amount)
+
+print("Increasing raise amount")
+
+Employee.raise_amount = 1.05
+
+print(Employee.raise_amount)
+print(emp_1.raise_amount)
+print(emp_2.raise_amount)
 
 
 
